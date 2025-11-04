ff35d681ae8ed4a92eed35e2c3d4493c255395ea
epenet <6771947+epenet@users.noreply.github.com>
2025-11-04 17:17:00 +0100

    Add quirk for cz_jti3ce2hzvsposgj

---
+++ src/tuya_device_handlers/devices/cz/jti3ce2hzvsposgj.py
@@ -0,0 +1,47 @@
+"""Quirks for Tuya."""
+
+from __future__ import annotations
+
+from tuya_device_handlers import TUYA_QUIRKS_REGISTRY
+from tuya_device_handlers.builder import TuyaDeviceQuirk
+from tuya_device_handlers.helpers import (
+    TuyaDeviceCategory,
+    TuyaDPCode,
+    TuyaSensorDeviceClass,
+    TuyaSensorStateClass,
+    TuyaSwitchDeviceClass,
+)
+
+(
+    TuyaDeviceQuirk()
+    .applies_to(category=TuyaDeviceCategory.CZ, product_id="jti3ce2hzvsposgj")
+    .add_sensor(
+        key=TuyaDPCode.CUR_CURRENT,
+        translation_key=None,
+        translation_string=None,
+        device_class=TuyaSensorDeviceClass.CURRENT,
+        state_class=TuyaSensorStateClass.MEASUREMENT,
+    )
+    .add_sensor(
+        key=TuyaDPCode.CUR_POWER,
+        translation_key=None,
+        translation_string=None,
+        device_class=TuyaSensorDeviceClass.POWER,
+        state_class=TuyaSensorStateClass.MEASUREMENT,
+    )
+    .add_sensor(
+        key=TuyaDPCode.CUR_VOLTAGE,
+        translation_key=None,
+        translation_string=None,
+        device_class=TuyaSensorDeviceClass.VOLTAGE,
+        state_class=TuyaSensorStateClass.MEASUREMENT,
+    )
+    .add_switch(
+        key=TuyaDPCode.SWITCH_1,
+        translation_key="indexed_switch",
+        translation_placeholders={"index": "1"},
+        translation_string="Switch {index}",
+        device_class=TuyaSwitchDeviceClass.OUTLET,
+    )
+    .register(TUYA_QUIRKS_REGISTRY)
+)
