5b8b7b119871dad207686be8cfaa1166bc3c8326
epenet <6771947+epenet@users.noreply.github.com>
2025-12-12 11:58:43 +0100

    More

--- tests/test_const.py
+++ tests/test_const.py
@@ -8,8 +8,22 @@ from tuya_device_handlers.const import DPType
 @pytest.mark.parametrize(
     ("current_type", "expected_type"),
     [
+        # Regular
         ("Boolean", DPType.BOOLEAN),
+        ("Enum", DPType.ENUM),
+        ("Integer", DPType.INTEGER),
+        ("Json", DPType.JSON),
+        ("Raw", DPType.RAW),
+        ("String", DPType.STRING),
+        # Special
+        ("bitmap", DPType.BITMAP),
         ("bool", DPType.BOOLEAN),
+        ("enum", DPType.ENUM),
+        ("json", DPType.JSON),
+        ("raw", DPType.RAW),
+        ("string", DPType.STRING),
+        ("value", DPType.INTEGER),
+        # Invalid
         ("boolbool", None),
     ],
 )
