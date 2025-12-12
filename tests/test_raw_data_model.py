6f299fee393d20f554c605674fbb6b4342b46ddd
epenet <6771947+epenet@users.noreply.github.com>
2025-12-12 08:46:48 +0100

    Add ElectricityData raw data model

---
+++ tests/test_raw_data_model.py
@@ -0,0 +1,37 @@
+"""Test utils"""
+
+import base64
+import dataclasses
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from tuya_device_handlers.raw_data_model import ElectricityData
+
+
+@pytest.mark.parametrize(
+    "base64_string",
+    [
+        "Ag8JJQAASAAACAAAAAAACGME",
+        "AAAAAAAAAAAAAA==",
+        "CGYAPCgADPIACw==",
+        "CIsAK8MACWo=",
+        "CJwAA5EAAFw=",
+        "CKMAAn0AAGw=",
+        "CPQAI58ACBA=",
+        "CREANUkADG8=",
+        "CSIAFfQABKE=",
+        "CT0AAmAAAIU=",
+        "CTIAVfcAFGw=",
+    ],
+)
+def test_electricity_data(
+    base64_string: str,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test ElectricityData."""
+    raw_bytes = base64.b64decode(base64_string)
+    raw_data = ElectricityData.from_bytes(raw_bytes)
+
+    assert raw_data is not None
+    assert dataclasses.asdict(raw_data) == snapshot
